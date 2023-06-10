8e95eecc39161e0d24223a70dee123d258c0a785
Towich <towichf@gmail.com>
2023-06-11 01:01:33 +0300

    Init

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.todoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAnchorView(R.id.fab)
+                    .setAction("Action", null).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/TodoItem.kt
@@ -0,0 +1,3 @@
+package com.example.todoapp
+
+data class TodoItem(val id: String, val textCase: String, )
