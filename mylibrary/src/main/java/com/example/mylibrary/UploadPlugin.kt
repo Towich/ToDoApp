a976d8cc9788bd4cdb73740b4b80a2a25eeedd30
Towich <towichf@gmail.com>
2023-08-07 22:27:18 +0400

    feat: renamed output .apk file ~ Now: "todolist-{debug/release}-{versionName}"

--- mylibrary/src/main/java/com/example/mylibrary/UploadPlugin.kt
+++ mylibrary/src/main/java/com/example/mylibrary/UploadPlugin.kt
@@ -4,27 +4,23 @@ import com.android.build.api.artifact.SingleArtifact
 import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.provider.Property
-import java.io.File
 
 class UploadPlugin : Plugin<Project> {
-    private val validateTaskActive = true
-    private val apkMaxSize = 10.0
-
     override fun apply(project: Project) {
-        val extension = project.extensions.create("uploadExtension", UploadPluginExtension::class.java)
-        extension.validateTaskActive.set(this.validateTaskActive)
-        extension.maxApkSize.set(this.apkMaxSize)
+        // Create an extension
+        project.extensions.create("uploadExtension", UploadPluginExtension::class.java)
 
         val androidComponents =
             project.extensions.findByType(AndroidComponentsExtension::class.java)
                 ?: throw NullPointerException("~ AndroidComponentsExtension not found!")
 
+        // On each variant
         androidComponents.onVariants { variant ->
 
             val variantName = variant.name.capitalize()
             val apkFileFromVariant = variant.artifacts.get(SingleArtifact.APK)
 
+            // Task validateApkSizeFor*
             project.tasks.register(
                 "validateApkSizeFor$variantName",
                 ValidateApkSizeTask::class.java
@@ -33,9 +29,12 @@ class UploadPlugin : Plugin<Project> {
                 apkSizeFile.set(project.layout.buildDirectory.file("apk-size"))
             }
 
+            // Task uploadApkFor*
             project.tasks.register("uploadApkFor$variantName", UploadTask::class.java) {
-                apkDir.set(apkFileFromVariant)
-                apkSizeFile.set(project.layout.buildDirectory.file("apk-size"))
+                this.apkDir.set(apkFileFromVariant)
+                this.apkSizeFile.set(project.layout.buildDirectory.file("apk-size"))
+
+                // Dependency on validateApkSizeFor*
                 dependsOn(":app:validateApkSizeFor$variantName")
             }
         }
--- mylibrary/src/main/java/com/example/mylibrary/UploadTask.kt
+++ mylibrary/src/main/java/com/example/mylibrary/UploadTask.kt
@@ -6,9 +6,12 @@ import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 import kotlin.math.roundToInt
 
 abstract class UploadTask: DefaultTask() {
@@ -25,12 +28,16 @@ abstract class UploadTask: DefaultTask() {
         val token = Confidential.token
         val chatId = Confidential.chatId
 
+        // Execute a thread
         runBlocking {
             apkDir.get().asFile.listFiles()
                 .filter { it.name.endsWith(".apk")}
                 .forEach { apkFile ->
-                    api.uploadFile(apkFile, token, chatId)  // upload file to chat in Telegram
-                    api.sendMessage("APK size: ${apkSizeFile.get().asFile.readText()} MB", token, chatId)  // send message in Telegram
+                    // Upload file to chat in Telegram
+                    api.uploadFile(apkFile, token, chatId)
+
+                    // Send message in Telegram
+                    api.sendMessage("APK size: ${apkSizeFile.get().asFile.readText()} MB", token, chatId)
                 }
         }
     }
